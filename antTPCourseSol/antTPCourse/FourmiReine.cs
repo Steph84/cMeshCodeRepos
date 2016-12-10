7a7fad154a49528d510bb6f061e7416af3940e95
Steph84 <st_lerendu@yahoo.fr>
2016-12-10 22:17:40 +0100

    queen in a thread laying eggs

--- antTPCourseSol/antTPCourse/FourmiReine.cs
+++ antTPCourseSol/antTPCourse/FourmiReine.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace antTPCourse
 {
@@ -13,25 +15,40 @@ namespace antTPCourse
         private List<Oeufs> eggsList = new List<Oeufs>();   
 
 
-        public FourmiReine(int pId)
+        public FourmiReine(int pId, int pNumEggs)
         {
             name = "Queen";
             id = pId;
             coordX = 0;
             coordY = 0;
+            numEggs = pNumEggs;
         }
 
         internal void Ponte(int pEggs, int pX, int pY)
         {
             int i = 0;
-            for (i = 0; i < (pEggs + 1); i++)
+            for (i = 0; i < pEggs; i++)
             {
                 FourmiSoldat monSoldat = new FourmiSoldat(( i + 1 ), pX, pY);
                 eggsList.Add(monSoldat);
+                Console.WriteLine("Warrior egg number " + (i + 1) + " laid !");
             }
             
-            Console.WriteLine("Ca fonctionne");
             
         }
+
+        internal void queenRun()
+        {
+            Console.WriteLine("I am alive as an ant queen !");
+            Ponte(numEggs, coordX, coordY);
+            while (true)
+            {
+
+                Thread.Sleep(500);
+                Console.WriteLine("Ant Queen alive ...");
+            }
+        }
+        
+         
     }
 }
--- antTPCourseSol/antTPCourse/Program.cs
+++ antTPCourseSol/antTPCourse/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace antTPCourse
 {
@@ -13,7 +15,11 @@ namespace antTPCourse
 
             Console.WriteLine("Hello !");
 
-            FourmiReine myQueen = new FourmiReine(1);
+            FourmiReine myQueen = new FourmiReine(1, 10);
+            Thread queenThread = new Thread ( new ThreadStart ( myQueen.queenRun ) ); // thread object
+            queenThread.Start(); // start the thread
+            while (!queenThread.IsAlive);
+
 
             myQueen.ShowName();
             
