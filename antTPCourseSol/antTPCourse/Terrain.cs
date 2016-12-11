bd17fcb53236e35e0c7c96334f5f064f2a1b15a2
Steph84 <st_lerendu@yahoo.fr>
2016-12-12 00:00:12 +0100

    add first element of the terrain creation

---
+++ antTPCourseSol/antTPCourse/Terrain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antTPCourse
+{
+    class Terrain
+    {
+        internal int mapWidth;
+        internal int mapHeight;
+        internal int tileSize;
+
+        internal Terrain(int pWidth, int pHeight, int pTileSize)
+        {
+            mapWidth = pWidth;
+            mapHeight = pHeight;
+            tileSize = pTileSize;
+        }
+
+    }
+}
---
+++ antTPCourseSol/antTPCourse/Tile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antTPCourse
+{
+    class Tile
+    {
+        internal int id;
+        internal int coordX;
+        internal int coordY;
+        internal string stuff; // nothing, food, branch
+        internal int stuffQty;
+        internal int pheroRate;
+
+        internal Tile(int pId, int pX, int pY, string pStuff, int pStuffQty, int pPhero)
+        {
+            id = pId;
+            coordX = pX;
+            coordY = pY;
+            stuff = pStuff;
+            stuffQty = pStuffQty;
+            pheroRate = pPhero;
+        }
+
+        internal void Evaporation()
+        {
+            if (pheroRate > 0)
+            {
+                pheroRate = pheroRate - 5;
+            }
+            else
+            {
+                pheroRate = 0;
+            }
+        }
+    }
+}
