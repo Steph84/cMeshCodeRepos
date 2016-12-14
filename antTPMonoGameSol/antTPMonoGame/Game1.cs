4fe3ff0cec4c0779a320617d1637ebeaddcaf50f
Steph84 <st_lerendu@yahoo.fr>
2016-12-14 23:09:39 +0100

    add new project moving ant on the screen

---
+++ antTPMonoGameSol/antTPMonoGame/Game1.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace antTPMonoGame
+{
+   
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D antPic; // declarer le membre
+        int picWidth;
+        int picHeight;
+        Vector2 picOrigin;
+        Vector2 antPosition;
+        int antRot;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+        
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            antPic = this.Content.Load<Texture2D>("ant01");
+            picWidth = antPic.Width;
+            picHeight = antPic.Height;
+            picOrigin = new Vector2(picWidth / 2, picHeight / 2);
+
+            antPosition = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+            antRot = 0;
+
+        }
+        
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+        
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+
+            base.Update(gameTime);
+        }
+        
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Green); // background color
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(antPic, antPosition, null, Color.White, antRot, picOrigin, 1.0f, SpriteEffects.None, 0);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
