cf51da572f57c091cadef4411529f1e4ae959ff8
Pirate Captain <st_lerendu@yahoo.fr>
2017-02-07 22:53:03 +0100

    car moving and turning

--- raceGame01Sol/raceGame01/Game1.cs
+++ raceGame01Sol/raceGame01/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace raceGame01
 {
@@ -14,7 +15,10 @@ namespace raceGame01
         Vector2 posMap;
         Vector2 posCar;
         Vector2 originCar;
+        Vector2 speed = Vector2.Zero;
         int spriteSize = 128;
+        float carDir = 0;
+        int maxSpeed = 10;
 
         public Game1()
         {
@@ -29,7 +33,7 @@ namespace raceGame01
             float initY = -6.5f;
             posMap = new Vector2(initX*spriteSize, initY*spriteSize);
             posCar = new Vector2(GraphicsDevice.Viewport.Width/2, GraphicsDevice.Viewport.Height/2);
-            originCar = new Vector2(car01.Width / 2, car01.Height / 2);
+            originCar = new Vector2(car01.Width / 2, car01.Height / 4);
         }
         
         protected override void LoadContent()
@@ -51,6 +55,26 @@ namespace raceGame01
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             
+            KeyboardState state = Keyboard.GetState();
+            
+            if (state.IsKeyDown(Keys.Right))
+                carDir += 0.1f;
+            if (state.IsKeyDown(Keys.Left))
+                carDir -= 0.1f;
+            if (state.IsKeyDown(Keys.Up))
+            {
+                speed.X = (float)(maxSpeed * Math.Sin(-carDir));
+                speed.Y = (float)(maxSpeed * Math.Cos(-carDir));
+                posMap.X = posMap.X + speed.X;
+                posMap.Y = posMap.Y + speed.Y;
+            }
+                
+            if (state.IsKeyDown(Keys.Down))
+                posMap.Y -= maxSpeed;
+
+
+
+
             base.Update(gameTime);
         }
 
@@ -60,7 +84,7 @@ namespace raceGame01
 
             spriteBatch.Begin();
             spriteBatch.Draw(map01, posMap);
-            spriteBatch.Draw(car01, posCar, null, Color.White, 0, originCar, 1.0f, SpriteEffects.None, 0);
+            spriteBatch.Draw(car01, posCar, null, Color.White, carDir, originCar, 1.0f, SpriteEffects.None, 0);
             spriteBatch.End();
 
             base.Draw(gameTime);
