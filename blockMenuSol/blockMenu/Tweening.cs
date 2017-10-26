830c3fe8b0155da3809748efe393b9e64368103b
Pirate Captain <st_lerendu@yahoo.fr>
2017-10-26 13:54:46 +0200

    add tweening file

---
+++ blockMenuSol/blockMenu/Tweening.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace blockMenu
+{
+    public class Tweening
+    {
+        public double EaseOutSin(double t, double b, double c, double d)
+        {
+            double temp = 0;
+            temp = c * Math.Sin(t / d * (Math.PI / 2)) + b;
+            return temp;
+        }
+    }
+}
