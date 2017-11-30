1f61d0cb4c37bad81e759b5b127003f9c2b98ad6
Pirate Captain <st_lerendu@yahoo.fr>
2017-11-30 13:53:02 +0100

    change name in AssemblyInfo

--- jamGitHubGameOffSol/jamGitHubGameOff/Properties/AssemblyInfo.cs
+++ jamGitHubGameOffSol/jamGitHubGameOff/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("blockMenu")]
-[assembly: AssemblyProduct("blockMenu")]
+[assembly: AssemblyTitle("jamGitHubGameOff")]
+[assembly: AssemblyProduct("jamGitHubGameOff")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
