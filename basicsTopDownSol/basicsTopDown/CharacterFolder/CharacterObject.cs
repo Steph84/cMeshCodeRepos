eef792d96674fe3c5b2707b02e96d4100f306df5
Pirate Captain <st_lerendu@yahoo.fr>
2018-02-05 22:13:19 +0100

    begin walking animation

--- basicsTopDownSol/basicsTopDown/CharacterFolder/CharacterObject.cs
+++ basicsTopDownSol/basicsTopDown/CharacterFolder/CharacterObject.cs
@@ -5,6 +5,16 @@ using System;
 
 namespace basicsTopDown.CharacterFolder
 {
+    [Flags]
+    public enum EnumCharacterDirection
+    {
+        None = 0,
+        North = 1,
+        East = 2,
+        South = 4,
+        West = 8,
+    }
+
     public class CharacterObject : SpriteObject
     {
         public string Name { get; set; }
@@ -12,6 +22,12 @@ namespace basicsTopDown.CharacterFolder
         public Rectangle FrameSize { get; set; }
         public int FrameNumber { get; set; }
         public Rectangle SourceQuad { get; set; }
+        public EnumCharacterDirection Direction { get; set; }
+
+        // for animation 
+        private double CurrentFrame { get; set; }
+        private double SpeedAnimation { get; set; }
+        private SpriteEffects SpriteDirection { get; set; }
 
         Vector2 SpriteOrigin = new Vector2();
 
@@ -21,6 +37,12 @@ namespace basicsTopDown.CharacterFolder
         {
             FrameSize = pFrameSize;
             SpriteData = Content.Load<Texture2D>(pSpriteName);
+            Direction = EnumCharacterDirection.East;
+
+            // for animation
+            CurrentFrame = 0;
+            SpeedAnimation = 1;
+            SpriteDirection = SpriteEffects.None;
 
             #region Determine the line number
             // an animation tileSet is made of 3 or 4 lines
@@ -29,7 +51,7 @@ namespace basicsTopDown.CharacterFolder
             // 3 animation toward bottom
             // 4 if exist animation toward left
 
-            if(SpriteData.Height / FrameSize.Height == 3)
+            if (SpriteData.Height / FrameSize.Height == 3)
             {
                 HasMirror = true;
             }
@@ -61,10 +83,61 @@ namespace basicsTopDown.CharacterFolder
             Position = new Rectangle(pPosition.X, pPosition.Y, spriteWidthShowing, spriteHeightShowing);
             SourceQuad = new Rectangle(0, 0, FrameSize.Width, FrameSize.Height);
         }
+
+        public void CharacterUpdate(GameTime pGameTime)
+        {
+            // en fonction de la direction, quelle ligne choisir
+            if(IsMoving)
+            {
+
+            }
+            else
+            {
+                if(HasMirror) // if there is only 3 lines
+                {
+                    if(Direction == EnumCharacterDirection.West)
+                    {
+                        SpriteDirection = SpriteEffects.FlipHorizontally; // correct the Direction
+                    }
+                    else
+                    {
+                        SpriteDirection = SpriteEffects.None;
+                    }
+                }
+                else
+                {
+                    // nothing to do
+                }
+                SourceQuad = new Rectangle(0, (int)Direction, SourceQuad.Width, SourceQuad.Height);
+            }
+            
+
+            switch (Direction)
+            {
+                case EnumCharacterDirection.None:
+                    break;
+                case EnumCharacterDirection.North:
+                    SourceQuad = new Rectangle(0, 0, SourceQuad.Width, SourceQuad.Height);
+                    break;
+                case EnumCharacterDirection.East:
+                    SourceQuad = new Rectangle(0, 1 * FrameSize.Height, SourceQuad.Width, SourceQuad.Height);
+                    break;
+                case EnumCharacterDirection.South:
+                    SourceQuad = new Rectangle(0, 0, SourceQuad.Width, SourceQuad.Height);
+                    break;
+                case EnumCharacterDirection.West:
+                    SourceQuad = new Rectangle(0, 0, SourceQuad.Width, SourceQuad.Height);
+                    break;
+                default:
+                    break;
+            }
+
+            // en fontion du temps quelle frame choisir
+        }
         
         public void CharacterDraw(GameTime pGameTime)
         {
-            SpriteBatch.Draw(SpriteData, Position, SourceQuad, Color.White, 0, SpriteOrigin, SpriteEffects.None, 0);
+            SpriteBatch.Draw(SpriteData, Position, SourceQuad, Color.White, 0, SpriteOrigin, SpriteDirection, 0);
         }
     }
 }
