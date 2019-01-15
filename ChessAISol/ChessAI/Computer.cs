7d1b69004220867cecaf4666af293356bab73bf4
Pirate Captain <st_lerendu@yahoo.fr>
2019-01-15 17:43:44 +0100

    begin compute density

--- ChessAISol/ChessAI/Computer.cs
+++ ChessAISol/ChessAI/Computer.cs
@@ -43,6 +43,7 @@ namespace ChessAI
             ListPossibleBlackMoves = new List<PossibleMove>();
             ListPossibleWhiteMoves = new List<PossibleMove>();
 
+            #region get possible moves foreach side
             for (int row = 0; row < ChessBoard.RowNumber; row++)
             {
                 for (int column = 0; column < ChessBoard.ColumnNumber; column++)
@@ -88,6 +89,59 @@ namespace ChessAI
                     }
                 }
             }
+            #endregion
+
+            #region manage density
+            #region Current Density
+            var currentDensity = 0;
+            for (int row = 0; row < ChessBoard.RowNumber; row++)
+            {
+                ChessBoard.BoardSquare[] sqrRow = Enumerable.Range(0, ChessBoard.Board.GetLength(1))
+                .Select(x => ChessBoard.Board[row, x])
+                .ToArray();
+
+                int pieceNbByRow = sqrRow.Where(x => x.Piece != null && x.Piece.PieceColor == PieceColors.Black).Count();
+                currentDensity += (pieceNbByRow * (row + 1))/ ChessBoard.ColumnNumber;
+            }
+            #endregion
+            #region Possible Densities
+            // pour chaque possible move black
+            // créer nouveau chess board temporaire
+            // calculer la densité possible
+
+            #endregion
+            #endregion
+
+            #region check the booleans WillBeEaten and WillEat
+            //if (ListPossibleBlackMoves.Count > 0)
+            //{
+            //    List<Point> listCurrentPosWhite = ListPossibleWhiteMoves.Select(x => x.From).Distinct().ToList();
+            //    //List<Point> listPossiblePosWhite = ListPossibleWhiteMoves.Select(x => x.To).Distinct().ToList();
+
+            //    foreach (PossibleMove posMov in ListPossibleBlackMoves)
+            //    {
+            //        //if (listPossiblePosWhite.Contains(posMov.To))
+            //        //{
+            //        //    posMov.WillBeEaten = true;
+            //        //}
+
+            //        if (listCurrentPosWhite.Contains(posMov.To))
+            //        {
+            //            posMov.WillEat = true;
+            //        }
+            //    }
+            //}
+            #endregion
+
+            #region Compute Probability Rate
+            // en fonction de la dnesité et des boolean et d'autres choses peut être...
+            #endregion
+
+            #region Pick a Move
+            // dans la liste, en fonction du taux, choisir un mouvement
+            #endregion
+
+            pTurn = GameRun.PlayerTurn.Player;
             return pTurn;
         }
     }
