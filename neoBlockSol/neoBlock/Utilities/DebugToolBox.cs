5d38190417efb2fcc6122631f289866eddbd3d2b
Pirate Captain <st_lerendu@yahoo.fr>
2019-11-09 09:36:27 +0100

    add debugToolBox

---
+++ neoBlockSol/neoBlock/Utilities/DebugToolBox.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+public class DebugToolBox
+{
+    public static void ShowLine(string pText, Vector2 pPosition)
+    {
+        SpriteFont tempFont = Main.GlobalContent.Load<SpriteFont>("TimesNewRoman12");
+
+        Main.GlobalSpriteBatch.DrawString(tempFont, pText, new Vector2(pPosition.X - 5, pPosition.Y - 12), Color.Black);
+    }
+}
