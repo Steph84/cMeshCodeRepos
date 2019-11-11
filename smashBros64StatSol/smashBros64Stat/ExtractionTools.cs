b612b6ddd3a7abd378bfd159b5c37d0365cf7869
Pirate Captain <st_lerendu@yahoo.fr>
2019-11-11 16:24:32 +0100

    smashBros dto and initialize

---
+++ smashBros64StatSol/smashBros64Stat/ExtractionTools.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+class ExtractionTools
+{
+    public enum Stage
+    {
+        Peach, // Château de Peach
+        Jungle, // Jungle du Congo
+        Hyrule, // Château d'Hyrule
+        Zebes, // Planète Zebes
+        Yoshi, // Ile Yoshi
+        Reves, // Pays des Rêves
+        SecteurZ,
+        Safrania,
+        Champignon, //Royaume Champignon
+        None
+    }
+
+    public enum Character
+    {
+        Luigi = 1,
+        Mario,
+        DonkeyKong,
+        Link,
+        SamusAran,
+        CFalcon,
+        Ness,
+        Yoshi,
+        Kirby,
+        Fox,
+        Pikachu,
+        Rondoudou
+    }
+
+    public class DataByStage
+    {
+        public Stage Stage { get; set; }
+        public List<DataByCharacter> ListDataByCharacter { get; set; }
+    }
+
+    public class DataByCharacter
+    {
+        public Character Character { get; set; }
+        public int Win { get; set; }
+        public int Lose { get; set; }
+        public int Tie { get; set; }
+        public int KO { get; set; }
+        public int TKO { get; set; }
+    }
+}
--- smashBros64StatSol/smashBros64Stat/Program.cs
+++ smashBros64StatSol/smashBros64Stat/Program.cs
@@ -2,33 +2,84 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static ExtractionTools;
 
-namespace smashBros64Stat
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Compute N64 Smash Bros data");
+
+        Dictionary<string, Stage> DictDataToStage = new Dictionary<string, Stage>()
         {
-            Console.WriteLine("Compute N64 Smash Bros data");
+            {"pea", Stage.Peach},
+            {"jun", Stage.Jungle},
+            {"hyr", Stage.Hyrule},
+            {"zeb", Stage.Zebes },
+            {"yos", Stage.Yoshi},
+            {"rev", Stage.Reves},
+            {"sec", Stage.SecteurZ},
+            {"saf", Stage.Safrania},
+            {"cha", Stage.Champignon}
+        };
 
-            using (var reader = new StreamReader("data.csv"))
+        #region Initialize FullData
+        List<DataByStage> FullData = new List<DataByStage>();
+        foreach (Stage stage in Enum.GetValues(typeof(Stage)))
+        {
+            DataByStage tempDataByStage = new DataByStage()
             {
-                List<string> listA = new List<string>();
-                List<string> listB = new List<string>();
-                while (!reader.EndOfStream)
+                Stage = stage,
+                ListDataByCharacter = new List<DataByCharacter>()
+            };
+
+            foreach (Character charac in Enum.GetValues(typeof(Character)))
+            {
+                DataByCharacter tempDataByCharacter = new DataByCharacter()
                 {
-                    var line = reader.ReadLine();
-                    var values = line.Split(';');
-
-                    listA.Add(values[0]);
-                    listB.Add(values[1]);
-                }
+                    Character = charac,
+                    Win = 0,
+                    Lose = 0,
+                    Tie = 0,
+                    KO = 0,
+                    TKO = 0
+                };
+                tempDataByStage.ListDataByCharacter.Add(tempDataByCharacter);
             }
-
-
-            Console.ReadKey();
+            
+            FullData.Add(tempDataByStage);
         }
+        #endregion
+
+        using (var reader = new StreamReader("../../data.csv"))
+        {
+            int line_count = 0;
+            while (!reader.EndOfStream)
+            {
+                line_count++;
+                Stage currentStage = Stage.None;
+                
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+
+                // first line or multiple of 14
+                if (line_count == 1 || line_count % 14 == 0)
+                {
+                    currentStage = DictDataToStage[values[0]];
+                }
+                else
+                {
+                    DataByStage dataToUpdate = FullData.Where(x => x.Stage == currentStage).First();
+
+                }
+
+
+                
+                
+            }
+        }
+
+
+        Console.ReadKey();
     }
 }
