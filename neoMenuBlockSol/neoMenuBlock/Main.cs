497f34af984c0b640c011075cb4717cf0e01f57d
Pirate Captain <st_lerendu@yahoo.fr>
2020-02-18 20:15:42 +0100

    add fullScreen in parameter resize window

--- neoMenuBlockSol/neoMenuBlock/Main.cs
+++ neoMenuBlockSol/neoMenuBlock/Main.cs
@@ -40,7 +40,7 @@ public class Main : Game
         GlobalCurrentDisplayMode = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode;
         GlobalViewport = GraphicsDevice.Viewport;
         GlobalGameWindow = Window;
-        MyWindow = new WindowDimension();
+        MyWindow = new WindowDimension(true);
 
         base.Initialize();
     }
