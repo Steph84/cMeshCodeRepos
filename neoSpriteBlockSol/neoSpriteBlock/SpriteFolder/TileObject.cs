9fe82e4d3114b66b63b9f44f7d5c40b0a0aecf30
Pirate Captain <st_lerendu@yahoo.fr>
2020-02-21 23:44:50 +0100

    add TileObject

---
+++ neoSpriteBlockSol/neoSpriteBlock/SpriteFolder/TileObject.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+class TileObject: SpriteObject
+{
+    public int Id { get; set; }
+    public Vector2 Coordinate { get; set; }
+
+    public TileObject(Rectangle pPosition, string pSpriteName) : base(pPosition, pSpriteName)
+    {
+        Position = pPosition;
+    }
+}
