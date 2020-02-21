1e6b143cbd11666ce6e4d8ce4765a2c17f3c98cf
Pirate Captain <st_lerendu@yahoo.fr>
2020-02-21 19:39:04 +0100

    add spriteObject class

---
+++ neoSpriteBlockSol/neoSpriteBlock/SpriteFolder/SpriteObject.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+class SpriteObject
+{
+    public Rectangle Position { get; set; }
+
+    protected Texture2D SpriteData { get; set; }
+    protected Rectangle SourceQuad { get; set; }
+    protected SpriteEffects SpriteEffect { get; set; }
+
+    private Vector2 SpriteOrigin { get; set; }
+
+    public SpriteObject(Rectangle pPosition, string pSpriteName)
+    {
+        SpriteOrigin = new Vector2();
+    }
+
+    #region SpriteUpdate
+    public virtual void SpriteUpdate(GameTime pGameTime) { }
+    #endregion
+
+    #region SpriteDraw
+    public virtual void SpriteDraw(GameTime pGameTime) { }
+    #endregion
+}
