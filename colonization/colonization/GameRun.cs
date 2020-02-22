afda99ae3c9b34d2d3d7be54e7f185b8e353dc1a
Pirate Captain <st_lerendu@yahoo.fr>
2020-02-22 17:47:24 +0100

    add gameRun and Map - pending

---
+++ colonization/colonization/GameRun.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+
+class GameRun
+{
+    public Map MyMap { get; set; }
+
+    public GameRun()
+    {
+        MyMap = new Map("testMapBitMap", "wallsTopDownTileSet", 96, 96);
+    }
+
+    public Main.EnumMainState GameRunUpdate(GameTime pGameTime, Main.EnumMainState pMyState)
+    {
+        MyMap.MapUpdate(pGameTime);
+        return pMyState;
+    }
+
+    public void GameRunDraw(GameTime pGameTime)
+    {
+        MyMap.MapDraw(pGameTime);
+    }
+}
