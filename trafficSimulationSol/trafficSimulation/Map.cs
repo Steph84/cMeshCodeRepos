61ab808d164f61be55177ee7af2334763e0ffbcc
Pirate Captain <st_lerendu@yahoo.fr>
2020-04-23 18:58:14 +0200

    change map size for hud

--- trafficSimulationSol/trafficSimulation/Map.cs
+++ trafficSimulationSol/trafficSimulation/Map.cs
@@ -94,7 +94,7 @@ public partial class Tile
 public static class Constantes
 {
     public const int RowNumber = 20;
-    public const int ColumnNumber = 36;
+    public const int ColumnNumber = 32; // 36 - 4 (hud)
     public const int SquareNumbers = 720;
     public const int SquareSize = 32;
 }
